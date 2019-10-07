59ad386e60236c0bfa6eab0dcd337e51abdd9ced
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-10-07 15:26:02 -0700

    added total list item to object 1

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+var createSalesList = document.getElementById('salesFigures');
+
 var numberOfCustomers = function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random()*(max - min + 1)) + min;
-}
-var createSalesList = document.getElementById('salesFigures');
+};
+
 
 var seattleCoffee = {
         storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
@@ -17,10 +19,21 @@ var seattleCoffee = {
                 seattleCoffee.cookiesPerHour.push((numberOfCustomers(seattleCoffee.min, seattleCoffee.max))*(seattleCoffee.averageCookiesPerCustomer));
                 var seattleSales = document.createElement('li');
                 createSalesList.appendChild(seattleSales);
-                seattleSales.textContent = `Shop Hours: ${this.storeHours[i]} ~~~ Cookies Purchased: ${this.cookiesPerHour[i]}`
+                seattleSales.textContent = `${this.storeHours[i]}: ${this.cookiesPerHour[i]} cookies`;
             }
+        },
+        totalCookiesPurch: 0,
+        totalCookiesPurchased: function(){
+            for(var y = 0; y < seattleCoffee.cookiesPerHour.length; y++){
+                this.totalCookiesPurch = this.totalCookiesPurch + this.cookiesPerHour[y];
+            }
+            var totalCookiesListItem = document.createElement('li');
+            createSalesList.appendChild(totalCookiesListItem);
+            totalCookiesListItem.textContent = `Total: ${this.totalCookiesPurch} cookies`;
         }
-    }
+    };
 seattleCoffee.numberOfCookiesNeeded();
-console.log(seattleCoffee.cookiesPerHour);
+seattleCoffee.totalCookiesPurchased();
+// console.log(seattleCoffee.totalCookiesPurch);
+// console.log(seattleCoffee.cookiesPerHour);
 // console.log(numberOfCustomers(seattleCoffee.min, seattleCoffee.max));
