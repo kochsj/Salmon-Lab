3357697f329cc9576715b9ce9df1a266bb9c8248
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-10-07 14:58:50 -0700

    Completed first object.

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ var numberOfCustomers = function(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random()*(max - min + 1)) + min;
 }
+var createSalesList = document.getElementById('salesFigures');
 
 var seattleCoffee = {
-    storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-    cookiesPerHour: [],
-    min: 2,
-    max: 20,
-    averageCookiesPerCustomer: 3,
-    numberOfCookiesNeeded: function() {
-        for(var i = 0; i < seattleCoffee.storeHours.length; i++){
-            seattleCoffee.cookiesPerHour.push((numberOfCustomers(seattleCoffee.min, seattleCoffee.max))*(seattleCoffee.averageCookiesPerCustomer));
+        storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+        cookiesPerHour: [],
+        min: 2,
+        max: 20,
+        averageCookiesPerCustomer: 3,
+
+        numberOfCookiesNeeded: function() {
+            for(var i = 0; i < seattleCoffee.storeHours.length; i++){
+                seattleCoffee.cookiesPerHour.push((numberOfCustomers(seattleCoffee.min, seattleCoffee.max))*(seattleCoffee.averageCookiesPerCustomer));
+                var seattleSales = document.createElement('li');
+                createSalesList.appendChild(seattleSales);
+                seattleSales.textContent = `Shop Hours: ${this.storeHours[i]} ~~~ Cookies Purchased: ${this.cookiesPerHour[i]}`
+            }
         }
-    }
     }
 seattleCoffee.numberOfCookiesNeeded();
 console.log(seattleCoffee.cookiesPerHour);
