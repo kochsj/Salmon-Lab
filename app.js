7169b0fc2b393ef389d8ec3a3b98483a62b01998
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-10-07 15:44:20 -0700

    made store hours a global variable. adjusted min/max for seattle store

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var createSalesList = document.getElementById('salesFigures');
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var numberOfCustomers = function(min, max) {
     min = Math.ceil(min);
@@ -6,20 +6,20 @@ var numberOfCustomers = function(min, max) {
     return Math.floor(Math.random()*(max - min + 1)) + min;
 };
 
-
+var createSeattleSalesList = document.getElementById('seattleSalesFigures');
 var seattleCoffee = {
-        storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+        // storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
         cookiesPerHour: [],
-        min: 2,
-        max: 20,
+        min: 10,
+        max: 60,
         averageCookiesPerCustomer: 3,
 
         numberOfCookiesNeeded: function() {
-            for(var i = 0; i < seattleCoffee.storeHours.length; i++){
+            for(var i = 0; i < storeHours.length; i++){
                 seattleCoffee.cookiesPerHour.push((numberOfCustomers(seattleCoffee.min, seattleCoffee.max))*(seattleCoffee.averageCookiesPerCustomer));
                 var seattleSales = document.createElement('li');
-                createSalesList.appendChild(seattleSales);
-                seattleSales.textContent = `${this.storeHours[i]}: ${this.cookiesPerHour[i]} cookies`;
+                createSeattleSalesList.appendChild(seattleSales);
+                seattleSales.textContent = `${storeHours[i]}: ${this.cookiesPerHour[i]} cookies`;
             }
         },
         totalCookiesPurch: 0,
@@ -28,7 +28,7 @@ var seattleCoffee = {
                 this.totalCookiesPurch = this.totalCookiesPurch + this.cookiesPerHour[y];
             }
             var totalCookiesListItem = document.createElement('li');
-            createSalesList.appendChild(totalCookiesListItem);
+            createSeattleSalesList.appendChild(totalCookiesListItem);
             totalCookiesListItem.textContent = `Total: ${this.totalCookiesPurch} cookies`;
         }
     };
@@ -37,3 +37,14 @@ seattleCoffee.totalCookiesPurchased();
 // console.log(seattleCoffee.totalCookiesPurch);
 // console.log(seattleCoffee.cookiesPerHour);
 // console.log(numberOfCustomers(seattleCoffee.min, seattleCoffee.max));
+
+// var createBellevueSalesList = document.getElementById('bellevueSalesFigures');
+// var bellevueCoffee = {
+//     min: 
+// }
+
+// var createRentonSalesList = document.getElementById('rentonSalesFigures');
+
+// var createEverettSalesList = document.getElementById('everettSalesFigures');
+
+// var createTacomaSalesList = document.getElementById('tacomaSalesFigures');
