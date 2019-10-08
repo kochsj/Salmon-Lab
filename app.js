beb1cc050e19e6bcc2cbbb7ad4c3ef3652c35135
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-10-08 12:05:09 -0700

    created the parent table elements in sales.html for my js file to append children to. Created a function to print all the store hours to table headings in sales.html

--- app.js
+++ app.js
@@ -1,49 +1,68 @@
-var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
 var numberOfCustomers = function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random()*(max - min + 1)) + min;
 };
 
-var CoffeeShop = function(cookiesPerHour, min, max, averageCookiesPerCustomer, totalCookiesPurch) {
-  this.cookiesPerHour = cookiesPerHour;
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var tableHeadingStoreHours = document.getElementById('firstSalesTableRow');
+var printTableHeadings = function() {
+  for(var a = 0; a < storeHours.length; a++) {
+    var tableStoreHours = document.createElement('th');
+    tableHeadingStoreHours.appendChild(tableStoreHours);
+    tableStoreHours.textContent = `${storeHours[a]}`;
+  }
+};
+printTableHeadings();
+
+
+var createSeattleSalesList = document.getElementById('seattleSalesFigures');
+
+var CoffeeShop = function(min, max, averageCookiesPerCustomer) {
+  this.cookiesPerHour = [];
   this.min = min;
   this.max = max;
   this.averageCookiesPerCustomer = averageCookiesPerCustomer;
-  this.totalCookiesPurch = totalCookiesPurch;
+  this.totalCookiesPurch = 0;
 };
 
-// CoffeeShop.prototype.numberOfCookiesNeeded = function() {
-//     for(var i = 0; i < storeHours.length; i++) {
-//         this.cookiesPerHour.push(Math.round((numberOfCustomers(this.min, this.max))*(this.averageCookiesPerCustomer)));
-//         var 
+CoffeeShop.prototype.numberOfCookiesNeeded = function() {
+  for(var i = 0; i < storeHours.length; i++) {
+    this.cookiesPerHour.push(Math.round((numberOfCustomers(this.min, this.max))*(this.averageCookiesPerCustomer)));
+    var storeSales = document.createElement('td');
+    createSeattleSalesList.appendChild(storeSales);
+    storeSales.textContent = `${storeHours[i]}: ${this.cookiesPerHour[i]} cookies`;
+    this.totalCookiesPurch += this.cookiesPerHour[i];
+  }
+  var totalCookiesListItem = document.createElement('li');
+  createSeattleSalesList.appendChild(totalCookiesListItem);
+  totalCookiesListItem.textContent = `Total: ${this.totalCookiesPurch} cookies`;
+};
+
+var seattleCoffee = new CoffeeShop(23, 65, 6.3);
+seattleCoffee.numberOfCookiesNeeded();
+
+
+// var seattleCoffee = {
+//   // storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
+//   cookiesPerHour: [],
+//   min: 23,
+//   max: 65,
+//   averageCookiesPerCustomer: 6.3,
+//   totalCookiesPurch: 0,
+
+//   numberOfCookiesNeeded: function() {
+//     for(var i = 0; i < storeHours.length; i++){
+//       seattleCoffee.cookiesPerHour.push(parseInt((numberOfCustomers(seattleCoffee.min, seattleCoffee.max))*(seattleCoffee.averageCookiesPerCustomer)));
+//       var seattleSales = document.createElement('li');
+//       createSeattleSalesList.appendChild(seattleSales);
+//       seattleSales.textContent = `${storeHours[i]}: ${this.cookiesPerHour[i]} cookies`;
+//       this.totalCookiesPurch += this.cookiesPerHour[i];
 //     }
-// }
-// var bellevueCoffee = new CoffeeShop([], 3, 40, 6, 0);
-// console.log(bellevueCoffee);
-
-var createSeattleSalesList = document.getElementById('seattleSalesFigures');
-var seattleCoffee = {
-  // storeHours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-  cookiesPerHour: [],
-  min: 23,
-  max: 65,
-  averageCookiesPerCustomer: 6.3,
-  totalCookiesPurch: 0,
-
-  numberOfCookiesNeeded: function() {
-    for(var i = 0; i < storeHours.length; i++){
-      seattleCoffee.cookiesPerHour.push(parseInt((numberOfCustomers(seattleCoffee.min, seattleCoffee.max))*(seattleCoffee.averageCookiesPerCustomer)));
-      var seattleSales = document.createElement('li');
-      createSeattleSalesList.appendChild(seattleSales);
-      seattleSales.textContent = `${storeHours[i]}: ${this.cookiesPerHour[i]} cookies`;
-      this.totalCookiesPurch += this.cookiesPerHour[i];
-    }
-    var totalCookiesListItem = document.createElement('li');
-    createSeattleSalesList.appendChild(totalCookiesListItem);
-    totalCookiesListItem.textContent = `Total: ${this.totalCookiesPurch} cookies`;
-  },
+//     var totalCookiesListItem = document.createElement('li');
+//     createSeattleSalesList.appendChild(totalCookiesListItem);
+//     totalCookiesListItem.textContent = `Total: ${this.totalCookiesPurch} cookies`;
+//   },
 //   totalCookiesPurchased: function(){
 //     for(var y = 0; y < seattleCoffee.cookiesPerHour.length; y++){
 //       this.totalCookiesPurch = this.totalCookiesPurch + this.cookiesPerHour[y];
@@ -52,9 +71,9 @@ var seattleCoffee = {
 //     createSeattleSalesList.appendChild(totalCookiesListItem);
 //     totalCookiesListItem.textContent = `Total: ${this.totalCookiesPurch} cookies`;
 //   }
-};
-seattleCoffee.numberOfCookiesNeeded();
-console.log(seattleCoffee.totalCookiesPurch);
+// };
+// seattleCoffee.numberOfCookiesNeeded();
+// console.log(seattleCoffee.totalCookiesPurch);
 // seattleCoffee.totalCookiesPurchased();
 // console.log(seattleCoffee.totalCookiesPurch);
 // console.log(seattleCoffee.cookiesPerHour);
